a547ad093d577ff35141aa0eb0bd42f6f4e11243
arkarmyomynn <144387917+arkarmyomynn@users.noreply.github.com>
2023-09-08 02:52:09 +0630

    Add files via upload

---
+++ My Library/template/bookui.php
@@ -0,0 +1,187 @@
+<?php 
+    echo '<div class="bookcontainer">
+    
+        <div class="book">
+            <img src="../img/Screenshot 2023-09-08 at 2.20.59 AM.png">
+            <div class="info">
+            <h4>ချစ်သောပါဒဧကရီ</h4>
+            <p>ချစ်ဦးညို</p>
+            <a href="../sever/data/ချစ်ဦးညို၊_ချစ်သောပါဒဧကရီ.pdf" download="ချစ်သောပါဒဧကရီ" class="download" >Download</a>
+            </div>
+        </div>
+        <div class="book">
+            <img src="../img/images.png">
+            <div class="info">
+            <h4>HTML+CSS reference</h4>
+            <p>supersimple.dev</p>
+            <a href="../sever/data/html-css-reference.pdf" download="HTML+CSS Reference" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/Screenshot 2023-09-07 at 11.25.59 PM.png">
+            <div class="info">
+            <h4>Professional Webdeveloper</h4>
+            <p>Ei Maung</p>
+            <a href="../sever/data/Professional-Web-Developer-2022.pdf" download="Professional Webdeveloper" class="download" >download</a>
+            </div>
+
+
+        </div>
+         <div class="book">
+            <img src="../img/Screenshot 2023-09-08 at 2.21.19 AM.png">
+            <div class="info">
+            <h4>ဗုဒ္ဓယဉ်ကျေးမှုဝတ္ထု</h4>
+            <p>ချစ်ဦးညို</p>
+            <a href="../sever/data/ချစ်ဦးညို၊_ဗုဒ္ဓယဉ်ကျေးမှုဝတ္ထု.pdf" download="ဗုဒ္ဓယဉ်ကျေးမှုဝတ္ထု" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >download</a>
+            </div>
+
+        </div>
+         <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>Book1</h4>
+            <p>မြသန်းတင့်</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >download</a>
+            </div>
+
+        </div>
+         <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>Book1</h4>
+            <p>မြသန်းတင့်</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >download</a>
+            </div>
+
+        </div>
+         <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>Book1</h4>
+            <p>မြသန်းတင့်</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >download</a>
+            </div>
+
+        </div>
+         <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>Book1</h4>
+            <p>မြသန်းတင့်</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >Download</a>
+            </div>
+
+        </div>
+        <div class="book">
+            <img src="../img/demon-slayer-kimetsu-no-yaiba-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg">
+            <div class="info">
+            <h4>BOOK1</h4>
+            <p>author</p>
+            <a href="" download="" class="download" >download</a>
+            </div>
+
+        </div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>';
+
+
+
+?>
---
+++ My Library/ui/index.html
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>our-library</title>
+    <link rel="stylesheet" href="../assets/css/mainui.css">
+</head>
+    
+
+<body>
+    <header>
+    <h1 class="greet">Good afternoon! Welcome to our library!</h1>
+    </header>
+
+    <div class="mainbody">
+
+    <?php 
+        include "../template/bookui.php"
+    ?>
+
+    </div>
+
+    <footer>
+        <p>Home</p>
+        <p>Categories</p>
+
+    </footer>
+
+    
+</body>
+<script src="../assets/js/script.js"></script>
+</html>
+
